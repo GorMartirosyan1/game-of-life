97b570c25e7b6aa2a74382b73b1433bf475f3457
GorMartirosyan1 <gor.martirosyan5@tumo.org>
2023-10-17 21:21:15 +0400

    lesson5

---
+++ server/server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static("../client"));
+
+app.get("/", function(req, res){
+
+res.redirect("index.html");
+
+    });
+
+server.listen(3000, function(){
+
+    console.log("Example is running on port 3000");
+
+    });
+
+    io.on('connection', function (socket) {
+
+        socket.emit("name", "Gor");
+        
+        
+        // socket.on("send message", function (data) {
+        
+        // messages.push(data);
+        
+        // io.sockets.emit("display message", data);
+        
+        // });
+        
+        });
